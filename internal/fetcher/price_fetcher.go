6aa0050d89e9e008318ef8fbadde417cef8280d3
Ramon Mesquita <ramonmesquita113@gmail.com>
2025-05-15 20:27:04 -0300

    format date and buffered channels

--- internal/fetcher/price_fetcher.go
+++ internal/fetcher/price_fetcher.go
@@ -1,14 +1,15 @@
 package fetcher
 
 import (
+	"buscador/internal/models"
 	"math/rand"
 	"sync"
 	"time"
 )
 
-func FetchPrices(priceChannel chan<- float64) {
+func FetchPrices(priceChannel chan<- models.PriceDetail) {
 	var wg sync.WaitGroup
-	wg.Add(3)
+	wg.Add(4)
 
 	go func() {
 		defer wg.Done()
@@ -25,21 +26,54 @@ func FetchPrices(priceChannel chan<- float64) {
 		priceChannel <- FetchPriceFromSite3()
 	}()
 
+	go func() {
+		defer wg.Done()
+		FetchAndSendMultiplePrices(priceChannel)
+	}()
+
 	wg.Wait()
 	close(priceChannel)
 }
 
-func FetchPriceFromSite1() float64 {
+func FetchPriceFromSite1() models.PriceDetail {
 	time.Sleep(1 * time.Second)
-	return rand.Float64() * 100
+	return models.PriceDetail{
+		StoreName: "A",
+		Value:     rand.Float64() * 100,
+		Timestamp: time.Now(),
+	}
 }
 
-func FetchPriceFromSite2() float64 {
+func FetchPriceFromSite2() models.PriceDetail {
 	time.Sleep(3 * time.Second)
-	return rand.Float64() * 100
+	return models.PriceDetail{
+		StoreName: "B",
+		Value:     rand.Float64() * 100,
+		Timestamp: time.Now(),
+	}
 }
 
-func FetchPriceFromSite3() float64 {
+func FetchPriceFromSite3() models.PriceDetail {
 	time.Sleep(2 * time.Second)
-	return rand.Float64() * 100
+	return models.PriceDetail{
+		StoreName: "C",
+		Value:     rand.Float64() * 100,
+		Timestamp: time.Now(),
+	}
+}
+
+func FetchAndSendMultiplePrices(priceChannel chan<- models.PriceDetail) {
+	time.Sleep(6 * time.Second)
+	prices := []float64{
+		rand.Float64() * 100,
+		rand.Float64() * 100,
+		rand.Float64() * 100,
+	}
+	for _, price := range prices {
+		priceChannel <- models.PriceDetail{
+			StoreName: "D",
+			Value:     price,
+			Timestamp: time.Now(),
+		}
+	}
 }
--- internal/processor/show_prices.go
+++ internal/processor/show_prices.go
@@ -1,15 +1,19 @@
 package processor
 
-import "fmt"
+import (
+	"buscador/internal/models"
+	"fmt"
+)
 
-func ShowPriceAVG(priceChannel <-chan float64, done chan<- bool) {
+func ShowPriceAVG(priceChannel <-chan models.PriceDetail, done chan<- bool) {
 	var totalPrice float64
 	countPrices := 0.0
-	for price := range priceChannel {
-		totalPrice += price
+	for priceDetail := range priceChannel {
+		totalPrice += priceDetail.Value
 		countPrices++
 		avgPrice := totalPrice / countPrices
-		fmt.Printf("Preco recebido: R$ %.2f | Preco medio até agora: R$ %.2f \n", price, avgPrice)
+
+		fmt.Printf("[%s] Preco recebido de %s: R$ %.2f | Preco medio até agora: R$ %.2f \n", priceDetail.Timestamp.Format("02-01 15:04:05"), priceDetail.StoreName, priceDetail.Value, avgPrice)
 	}
 	done <- true
 }
